717c340eaa7162e0deeaa177a86780d5cc73a117
builtbyfaiz <229525323+builtbyfaiz@users.noreply.github.com>
2026-05-25 22:32:48 +0500

    Add render function; increase btn label fontsize

--- view/GameGUI.java
+++ view/GameGUI.java
@@ -44,6 +44,7 @@ public class GameGUI {
                 int buttonNumber = (i * 3) + j + 1;
 
                 JButton btn = new JButton(String.valueOf(buttonNumber));
+                btn.setFont(new Font("roboto",Font.BOLD, 72));
 
                 btn.setBackground(Color.GREEN);
                 btn.setHorizontalAlignment(SwingConstants.CENTER); // Makes button text centered #TEMP
@@ -55,8 +56,11 @@ public class GameGUI {
     }
 
     private void initGameArea() {
-        JPanel gameFrame = new JPanel(new GridLayout(3, 3, 3, 3));
-        gameFrame.setPreferredSize(new Dimension(400, 400));
+        int gameAreaWidth = (windowWidth * 7) / 10; // 70% of Window Width
+        int gameFrameSize = (gameAreaWidth * 7) / 10; // 70% of GameArea
+
+        JPanel gameFrame = new JPanel(new GridLayout(3, 3, 8, 8));
+        gameFrame.setPreferredSize(new Dimension(gameFrameSize, gameFrameSize));
 
         for (var row : grid_) {
             for (var btn : row) {
@@ -68,9 +72,12 @@ public class GameGUI {
     }
 
     public void render(Game game) {
-        char grid[][] = game.getGrid();
-        for (int i = 0; i < grid.length; i++) {
+        char characterGrid[][] = game.getGrid();
 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                grid_[i][j].setText(String.valueOf(characterGrid[i][j]));
+            }
         }
     }
 }
