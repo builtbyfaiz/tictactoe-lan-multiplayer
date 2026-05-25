1abddaf8a61df2fb2435a1079bf702e20a455f86
builtbyfaiz <229525323+builtbyfaiz@users.noreply.github.com>
2026-05-25 18:25:43 +0500

    Complete terminal Implementation

--- Main.java
+++ Main.java
@@ -1,14 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("TicTacToe");
-        frame.setSize(400, 300);
-        frame.setLayout(new FlowLayout());
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel displayGrid;
-    }    
+        TicTacToe ticTacToe = new TicTacToe();
+
+        while (true) {
+            System.out.print("Enter Input (1-9): ");
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+            sc.close();
+            ticTacToe.play(choice);
+        }
+
+        // JFrame frame = new JFrame("TicTacToe");
+        // frame.setSize(400, 300);
+        // frame.setLayout(new FlowLayout());
+        // frame.setVisible(true);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // JPanel displayGrid;
+    }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,29 @@
-import java.util.Scanner;
+class TicTacToe {
 
-class TicTacToe1 {
+    private char grid[][] = new char[3][3];
 
-    private char grid[][] = { { '-', '-', '-' },
-            { '-', '-', '-' },
-            { '-', '-', '-' } };
+    char voidMarker = '-';
+    char player1Marker = 'X';
+    char player2Marker = 'O';
+    char currentMarker = player1Marker;
 
-    int turn = 1;
-    char currentMarker = 'X';
+    public int turn = 1;
+    public boolean win = false;
 
-    public void printGrid() {
-        System.out.print("\033[H\033[2J"); //Clear terminal with escape sequences
-        System.out.flush();
+    public TicTacToe() {
+        initGrid();
+        printGrid();
+    }
+
+    private void initGrid() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                grid[i][j] = voidMarker;
+            }
+        }
+    }
+
+    private void printGrid() {
 
         System.out.println();
         for (char[] row : grid) {
@@ -22,44 +34,99 @@ class TicTacToe1 {
         }
         System.out.println();
     }
+    
+    private void checkWin() {
 
-    public void mark(int gridIndexToMark) {
-        if (gridIndexToMark >= 1 && gridIndexToMark <= 10) {
-            switch (gridIndexToMark) {
-                case 1: grid[0][0] = currentMarker; break;
-                case 2: grid[0][1] = currentMarker; break;
-                case 3: grid[0][2] = currentMarker; break;
-                case 4: grid[1][0] = currentMarker; break;
-                case 5: grid[1][1] = currentMarker; break;
-                case 6: grid[1][2] = currentMarker; break;
-                case 7: grid[2][0] = currentMarker; break;
-                case 8: grid[2][1] = currentMarker; break;
-                case 9: grid[2][2] = currentMarker; break;
-                default: break;
-            }
-            toggleTurn();
+        for (int i = 0; i < grid.length; i++) {
+
+            // Check Vertically
+            if (grid[i][0] == currentMarker)
+                if (grid[i][1] == currentMarker)
+                    if (grid[i][2] == currentMarker)
+                        win = true;
+
+            // Check Horizontally
+            if (grid[0][i] == currentMarker)
+                if (grid[1][i] == currentMarker)
+                    if (grid[2][i] == currentMarker)
+                        win = true;
         }
-        else {
-            System.out.println("Invalid Input, Please input 1-9");
+
+        // Check Primary Diagonal -> \
+        if (grid[0][0] == currentMarker)
+            if (grid[1][1] == currentMarker)
+                if (grid[2][2] == currentMarker)
+                    win = true;
+
+        // Check Secondary Diagonal -> /
+        if (grid[0][2] == currentMarker)
+            if (grid[1][1] == currentMarker)
+                if (grid[2][0] == currentMarker)
+                    win = true;
+
+        if (win)
+            System.out.println("Player" + turn + " Won, Press 0 to Reset Game");
+    }
+
+
+
+    private void resetGame() {
+        currentMarker = 'X';
+        turn = 1;
+        win = false;
+
+        initGrid();
+    }
+
+
+
+    private void handleInput(int choice) {
+        // Parse Input
+        int row = 0;
+        int col = 0;
+
+        switch (choice) {
+            case 1: row = 0; col = 0; break;
+            case 2: row = 0; col = 1; break;
+            case 3: row = 0; col = 2; break;
+            case 4: row = 1; col = 0; break;
+            case 5: row = 1; col = 1; break;
+            case 6: row = 1; col = 2; break; 
+            case 7: row = 2; col = 0; break; 
+            case 8: row = 2; col = 1; break; 
+            case 9: row = 2; col = 2; break; 
+            case 0: resetGame();
+
+            default:
+                System.out.print("Invalid Input, Enter (1-9): ");
+                break;
+        }
+
+        // Take Actions
+        mark(row, col);
+    };
+
+    private void mark(int row, int col) {
+        if (grid[row][col] == voidMarker) {
+            grid[row][col] = currentMarker;
+            toggleTurn();
+        } else {
+            System.out.println("Invalid Input, Already Marked");
         }
     }
 
-    public void play() {
+    private void render() {
         printGrid();
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Input (1-9): ");
-        mark(sc.nextInt());
     };
 
-    public void toggleTurn() {
+    private void toggleTurn() {
         turn = turn == 1 ? 2 : 1; // Toggles turn from 1 and 2 each time
         currentMarker = currentMarker == 'X' ? 'O' : 'X'; // Toggles the character
     }
-}
 
-public class TicTacToe {
-    public static void main(String[] args) {
-        TicTacToe1 tac = new TicTacToe1();
-        tac.printGrid();
+    public void play(int choice) {
+        handleInput(choice);
+        render();
+        checkWin();
     }
 }
