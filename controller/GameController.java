d9a72358c61bdbfa6b51f6cd3ec88d0e2eb1328d
builtbyfaiz <229525323+builtbyfaiz@users.noreply.github.com>
2026-05-27 03:46:27 +0500

    Add bindNetworkBtns method

--- controller/GameController.java
+++ controller/GameController.java
@@ -1,15 +1,33 @@
 package controller;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 import model.Game;
 import view.GameGUI;
+import network.GameClient;
+import network.GamePeer;
+import network.GameServer;
 
 public class GameController {
     private Game game;
     private GameGUI view;
-    
+    private boolean multiplayerMode = false;
+
+    private GamePeer network;
+
     public GameController(Game game, GameGUI view) {
         this.game = game;
-        this.view = view;    
+        this.view = view;
+
+        view.getWindow().addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                network.disconnect();
+            }
+        });
 
         bindEvents();
     }
@@ -17,14 +35,44 @@ public class GameController {
     private void bindEvents() {
         bindGridEvents();
         bindResetBtnEvents();
+        bindNetworkButtons();
     }
 
     private void bindResetBtnEvents() {
-        view.resetBtn.addActionListener(e -> {
+        view.getReseBtn().addActionListener(e -> {
             game.play(0);
             view.resetGridColors();
             view.setAlertLabel(null);
-            view.render(game);
+            view.renderGameGrid(game);
+        });
+    }
+
+    private void bindNetworkButtons() {
+        view.getServerBtn().addActionListener(e -> {
+            network = new GameServer();
+            String IP = "";
+            try {
+                IP = InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e1) {
+            }
+            view.setAlertLabel("<html>Waiting for client.<br>IP: " + IP + "</html>");
+            view.renderGameGrid(game);
+            boolean conneced = network.connect(null);
+            if(conneced)
+                view.setAlertLabel("<html>Successfully Connected.<br>Make first move.</html>");
+        });
+
+        view.getClientBtn().addActionListener(e -> {
+            network = new GameClient();
+
+            System.out.println("Enter Server IP to Connect to: ");
+            Scanner sc = new Scanner(System.in);
+            String IP = sc.next();
+            sc.close();
+
+            boolean connected = network.connect(IP);
+            if(connected)
+                view.setAlertLabel("Successfully connected to server with IP: " + IP);
         });
     }
 
@@ -34,7 +82,7 @@ public class GameController {
                 btn.addActionListener(e -> {
 
                     view.setAlertLabel(null);
-                    
+
                     try {
                         int choice = Integer.parseInt(btn.getText());
                         game.play(choice);
@@ -44,13 +92,13 @@ public class GameController {
 
                     if (game.win)
                         view.setAlertLabel("<html><center>Player " + game.getTurn()
-                    + " Won!<br>Please Reset Game</center></html>");
-                    
+                                + " Won!<br>Please Reset Game</center></html>");
+
                     view.setTurnLabel("Turn: Player-" + game.getTurn());
                     view.setScoreLabel(game.getScore());
                     view.updateInfoLabel(); // Internally update itself to align with latest score and turn
 
-                    view.render(game);
+                    view.renderGameGrid(game);
                 });
             }
         }
