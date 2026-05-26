09a4606c7dcd1c326752e4dc9878c2a69249bee4
builtbyfaiz <229525323+builtbyfaiz@users.noreply.github.com>
2026-05-26 20:35:45 +0500

    Add server connectivity methods

--- network/GameServer.java
+++ network/GameServer.java
@@ -1,11 +1,30 @@
 package network;
 
+import java.io.IOException;
+import java.net.*;
+
 public class GameServer implements NetworkInterface {
 
+    ServerSocket server;
+    Socket socket;
+
     public GameServer() {
+        try {
+            server = new ServerSocket(8080); // New Connection Listener on the port
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error opening server.");
+        } 
+
+        try {
+            String myIP = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.err.println("Error getting local IP");
+        } 
 
     }
-
     public void send(int data) {
 
     }
@@ -16,11 +35,22 @@ public class GameServer implements NetworkInterface {
     }
 
     public void connect() {
-
+        try {
+            socket = server.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error connecting to client.");
+        }
     }
 
     public void disconnect() {
-
+        try {
+            server.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error in closing server.");
+        }
     }
 
 }
