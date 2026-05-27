cf055a72db1ed05f86ce52c1951757a52964e158
builtbyfaiz <229525323+builtbyfaiz@users.noreply.github.com>
2026-05-27 19:35:03 +0500

    Create new State Models, Networkstate and turnstate

---
+++ model/NetworkState.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum NetworkState {
+    MULTIPLAYER_INIT,
+    SERVER_INIT,
+    CLIENT_INIT,
+    CONNECTED,
+    DISCONNECTED,
+    FAILED
+}
---
+++ model/TurnState.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum TurnState {
+    YOUR_TURN,
+    WAITING_FOR_OPPONENT,
+    SENDING_MOVE,
+    PROCESSING_MOVE
+}
